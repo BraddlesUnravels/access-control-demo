1f0f3dd8c726d423a5d29da109058efe854c53e6
Bradley Laskey <103235805+BraddlesUnravels@users.noreply.github.com>
2026-08-03 16:25:24 +1000

    revert: roll back broken commit (8b2ea0eba2d8443d44891335b596dfa8962ed770) (#3)

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
-import os from 'node:os';
 import type { NextConfig } from 'next';
-
-const isPrivateIpv4 = (address: string): boolean =>
-  address.startsWith('10.') ||
-  address.startsWith('192.168.') ||
-  /^172\.(1[6-9]|2\d|3[01])\./.test(address);
-
-const localDevOrigins = Object.values(os.networkInterfaces())
-  .flatMap((interfaces) => interfaces ?? [])
-  .filter(
-    (network) =>
-      network.family === 'IPv4' &&
-      !network.internal &&
-      isPrivateIpv4(network.address),
-  )
-  .map((network) => network.address);
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  allowedDevOrigins: localDevOrigins,
 };
 
 export default nextConfig;
